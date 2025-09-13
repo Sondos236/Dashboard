cad65963ec5a9a13959637143fe9d748cfe9dd8c
Sondos <sondostamer23@gmail.com>
2025-09-14 01:44:25 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-pink-700 font-bold text-[3rem] h-screen text-white flex justify-center items-center">
+      Sondos is a Queen 👑🎀
+    </div>
+  );
+}
+
+export default App;
